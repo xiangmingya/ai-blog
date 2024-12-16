d6fd2cace3b39b75e38689a4f47cffb370610ffa
Mr、湘 <xiangmingla@qq.com>
2024-12-16 20:20:30 +0800

    first commit

---
+++ themes/hugo-theme-dream/src/js/grid.js
@@ -0,0 +1,17 @@
+function initGrid() {
+  document.querySelectorAll('.dream-grid').forEach((grid) => {
+    const msnry = new Masonry(grid, {
+      itemSelector: '.dream-column',
+    })
+
+    if (grid.classList.contains('dream-grid-about')) {
+      // Export Masonry instance to global scope for about page,
+      // currently used for resizing the grid after Disqus comments are loaded.
+      window.aboutMasonry = msnry
+    }
+
+    imagesLoaded(grid, () => msnry.layout())
+  })
+}
+
+initGrid()
